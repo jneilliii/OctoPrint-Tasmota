a590a9f10a327394696a1950b167b4cd5bddc4ed
jneilliii <jneilliii+github@gmail.com>
2017-11-04 00:21:37 -0400

    0.1.0

---
+++ octoprint_tasmota/__init__.py
@@ -0,0 +1,191 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+from octoprint.server import user_permission
+import socket
+import json
+import time
+import logging
+import os
+import re
+
+class tasmotaPlugin(octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.AssetPlugin,
+                            octoprint.plugin.TemplatePlugin,
+							octoprint.plugin.SimpleApiPlugin,
+							octoprint.plugin.StartupPlugin):
+							
+	def __init__(self):
+		self._logger = logging.getLogger("octoprint.plugins.tasmota")
+		self._tasmota_logger = logging.getLogger("octoprint.plugins.tasmota.debug")
+							
+	##~~ StartupPlugin mixin
+	
+	def on_startup(self, host, port):
+		# setup customized logger
+		from octoprint.logging.handlers import CleaningTimedRotatingFileHandler
+		tasmota_logging_handler = CleaningTimedRotatingFileHandler(self._settings.get_plugin_logfile_path(postfix="debug"), when="D", backupCount=3)
+		tasmota_logging_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s"))
+		tasmota_logging_handler.setLevel(logging.DEBUG)
+
+		self._tasmota_logger.addHandler(tasmota_logging_handler)
+		self._tasmota_logger.setLevel(logging.DEBUG if self._settings.get_boolean(["debug_logging"]) else logging.INFO)
+		self._tasmota_logger.propagate = False
+	
+	def on_after_startup(self):
+		self._logger.info("Tasmota loaded!")
+	
+	##~~ SettingsPlugin mixin
+	
+	def get_settings_defaults(self):
+		return dict(
+			debug_logging = False,
+			arrSmartplugs = [{'ip':'','displayWarning':True,'idx':1,'warnPrinting':False,'gcodeEnabled':False,'gcodeOnDelay':0,'gcodeOffDelay':0,'autoConnect':True,'autoConnectDelay':10.0,'autoDisconnect':True,'autoDisconnectDelay':0,'sysCmdOn':False,'sysRunCmdOn':'','sysCmdOnDelay':0,'sysCmdOff':False,'sysRunCmdOff':'','sysCmdOffDelay':0,'currentState':'unknown','btnColor':'#808080'}],
+		)
+		
+	def on_settings_save(self, data):	
+		old_debug_logging = self._settings.get_boolean(["debug_logging"])
+
+		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+
+		new_debug_logging = self._settings.get_boolean(["debug_logging"])
+		if old_debug_logging != new_debug_logging:
+			if new_debug_logging:
+				self._tasmota_logger.setLevel(logging.DEBUG)
+			else:
+				self._tasmota_logger.setLevel(logging.INFO)
+				
+	def get_settings_version(self):
+		return 1
+		
+	def on_settings_migrate(self, target, current=None):
+		if current is None or current < self.get_settings_version():
+			# Reset plug settings to defaults.
+			self._logger.debug("Resetting arrSmartplugs for tasmota settings.")
+			self._settings.set(['arrSmartplugs'], self.get_settings_defaults()["arrSmartplugs"])
+		
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		return dict(
+			js=["js/tasmota.js"]
+		)
+		
+	##~~ TemplatePlugin mixin
+	
+	def get_template_configs(self):
+		return [
+			dict(type="navbar", custom_bindings=True),
+			dict(type="settings", custom_bindings=True)
+		]
+		
+	##~~ SimpleApiPlugin mixin
+	
+	def turn_on(self, plugip, plugidx):
+		self._tasmota_logger.debug("Turning on %s index %s." % (plugip, plugidx))
+		response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx) + "%20on").read().split()[2])
+		chk = response["POWER"]
+		if chk == "ON":
+			self.check_status(plugip)
+	
+	def turn_off(self, plugip, plugidx):
+		self._tasmota_logger.debug("Turning off %s index %s." % (plugip, plugidx))
+		response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx) + "%20off").read().split()[2])
+		chk = response["POWER"]
+		if chk == "OFF":
+			self.check_status(plugip)
+		
+	def check_status(self, plugip, plugidx):
+		self._tasmota_logger.debug("Checking status of %s index %s." % (plugip, plugidx))
+		if plugip != "":
+			response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx)).read().split()[2])
+			chk = response["POWER"]
+			if chk == "ON":
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="on",ip=plugip,idx=plugidx))
+			elif chk == "OFF":
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="off",ip=plugip,idx=plugidx))
+			else:
+				self._tasmota_logger.debug(response)
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown",ip=plugip,idx=plugidx))		
+	
+	def get_api_commands(self):
+		return dict(turnOn=["ip","idx"],turnOff=["ip","idx"],checkStatus=["ip","idx"],connectPrinter=[],disconnectPrinter=[],sysCommand=["cmd"])
+
+	def on_api_command(self, command, data):
+		if not user_permission.can():
+			from flask import make_response
+			return make_response("Insufficient rights", 403)
+        
+		if command == 'turnOn':
+			self.turn_on("{ip}".format(**data),"{idx}".format(**data))
+		elif command == 'turnOff':
+			self.turn_off("{ip}".format(**data),"{idx}".format(**data))
+		elif command == 'checkStatus':
+			self.check_status("{ip}".format(**data),"{idx}".format(**data))
+		elif command == 'connectPrinter':
+			self._tasmota_logger.debug("Connecting printer.")
+			self._printer.connect()
+		elif command == 'disconnectPrinter':
+			self._tasmota_logger.debug("Disconnecting printer.")
+			self._printer.disconnect()
+		elif command == 'sysCommand':
+			self._tasmota_logger.debug("Running system command %s." % "{cmd}".format(**data))
+			os.system("{cmd}".format(**data))
+			
+	##~~ Gcode processing hook
+	
+	def processGCODE(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if gcode:
+			if cmd.startswith("M80"):
+				plugip = re.sub(r'^M80\s?', '', cmd)
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown",gcodeon=True,ip=plugip))
+				self._tasmota_logger.debug("Received M80 command, attempting power on.")
+				return
+			elif cmd.startswith("M81"):
+				plugip = re.sub(r'^M81\s?', '', cmd)
+				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown",gcodeoff=True,ip=plugip))
+				self._tasmota_logger.debug("Received M81 command, attempting power off.")
+				return
+			else:
+				return
+			
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			tasmota=dict(
+				displayName="OctoPrint-Tasmota",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-Tasmota",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-Tasmota/archive/{target_version}.zip"
+			)
+		)
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "TP-Link Smartplug"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = tasmotaPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.processGCODE,
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
---
+++ octoprint_tasmota/static/js/tasmota.js
@@ -0,0 +1,249 @@
+/*
+ * View model for OctoPrint-Tasmota
+ *
+ * Author: jneilliii
+ * License: AGPLv3
+ */
+$(function() {
+    function tasmotaViewModel(parameters) {
+        var self = this;
+
+        self.settings = parameters[0];
+		self.loginState = parameters[1];
+
+		self.arrSmartplugs = ko.observableArray();
+		self.isPrinting = ko.observable(false);
+		
+		self.onBeforeBinding = function() {		
+			self.arrSmartplugs(self.settings.settings.plugins.tasmota.arrSmartplugs());
+        }
+		
+		self.onAfterBinding = function() {
+			self.checkStatuses();
+		}
+
+        self.onEventSettingsUpdated = function(payload) {
+			self.settings.requestData();
+			self.arrSmartplugs(self.settings.settings.plugins.tasmota.arrSmartplugs());
+		}
+		
+		self.onEventPrinterStateChanged = function(payload) {
+			if (payload.state_id == "PRINTING" || payload.state_id == "PAUSED"){
+				self.isPrinting(true);
+			} else {
+				self.isPrinting(false);
+			}
+		}
+		
+		self.addPlug = function() {
+			self.settings.settings.plugins.tasmota.arrSmartplugs.push({'ip':ko.observable(''),
+									'idx':ko.observable(1),
+									'displayWarning':ko.observable(true),
+									'warnPrinting':ko.observable(false),
+									'gcodeEnabled':ko.observable(false),
+									'gcodeOnDelay':ko.observable(0),
+									'gcodeOffDelay':ko.observable(0),
+									'autoConnect':ko.observable(true),
+									'autoConnectDelay':ko.observable(10.0),
+									'autoDisconnect':ko.observable(true),
+									'autoDisconnectDelay':ko.observable(0),
+									'sysCmdOn':ko.observable(false),
+									'sysRunCmdOn':ko.observable(''),
+									'sysCmdOnDelay':ko.observable(0),
+									'sysCmdOff':ko.observable(false),
+									'sysRunCmdOff':ko.observable(''),
+									'sysCmdOffDelay':ko.observable(0),
+									'currentState':ko.observable('unknown'),
+									'btnColor':ko.observable('#808080')});
+		}
+		
+		self.removePlug = function(row) {
+			self.settings.settings.plugins.tasmota.arrSmartplugs.remove(row);
+		}
+		
+		self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "tasmota") {
+                return;
+            }
+			
+			plug = ko.utils.arrayFirst(self.settings.settings.plugins.tasmota.arrSmartplugs(),function(item){
+				return item.ip() === data.ip;
+				}) || {'ip':data.ip,'currentState':'unknown','btnColor':'#808080'};
+			
+			if (data.gcodeon && plug.gcodeEnabled()) {
+				setTimeout(function(){self.turnOn(plug)},plug.gcodeOnDelay()*1000);
+				return false;
+			}
+			
+			if (data.gcodeoff && plug.gcodeEnabled()) {
+				setTimeout(function(){self.turnOff(plug)},plug.gcodeOffDelay()*1000);
+				return false;
+			}
+			
+			if (plug.currentState != data.currentState) {
+				plug.currentState(data.currentState)
+				switch(data.currentState) {
+					case "on":
+						plug.btnColor("#00FF00");
+						break;
+					case "off":
+						plug.btnColor("#FF0000");
+						break;
+					default:
+						plug.btnColor("#808080");
+						new PNotify({
+							title: 'TP-Link Smartplug Error',
+							text: 'Status ' + plug.currentState() + ' for ' + plug.ip() + '. Double check IP Address\\Hostname in Tasmota Settings.',
+							type: 'error',
+							hide: true
+							});
+				self.settings.saveData();
+				}
+			}
+        };
+		
+		self.toggleRelay = function(data) {
+			switch(data.currentState()){
+				case "on":
+					self.turnOff(data);
+					break;
+				case "off":
+					self.turnOn(data);
+					break;
+				default:
+					self.checkStatus(data.ip(),data.idx());
+			}
+		}
+		
+		self.turnOn = function(data) {
+			if(data.sysCmdOn()){
+				setTimeout(function(){self.sysCommand(data.sysRunCmdOn())},data.sysCmdOnDelay()*1000);
+			}
+			if(data.autoConnect()){
+				self.sendTurnOn(data);
+				setTimeout(function(){self.connectPrinter()},data.autoConnectDelay()*1000);
+			} else {
+				self.sendTurnOn(data);
+			}
+		}
+		
+		self.sendTurnOn = function(data) {
+            $.ajax({
+                url: API_BASEURL + "plugin/tasmota",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "turnOn",
+					ip: data.ip(),
+					idx: data.idx()
+                }),
+                contentType: "application/json; charset=UTF-8"
+            });
+        };
+
+    	self.turnOff = function(data) {
+			var dlg_id = "#tasmota_poweroff_confirmation_dialog_" + data.ip().replace( /(:|\.|[|])/g, "\\$1" + "_" + data.idx());
+			if((data.displayWarning() || (self.isPrinting() && data.warnPrinting())) && !$(dlg_id).is(':visible')){
+				$(dlg_id).modal("show");
+			} else {
+				$(dlg_id).modal("hide");
+				if(data.sysCmdOff()){
+					setTimeout(function(){self.sysCommand(data.sysRunCmdOff())},data.sysCmdOffDelay()*1000);
+				}
+				if(data.autoDisconnect()){
+					self.disconnectPrinter();
+					setTimeout(function(){self.sendTurnOff(data);},data.autoDisconnectDelay()*1000);
+				} else {
+					self.sendTurnOff(data);
+				}
+			}
+        }; 
+		
+		self.sendTurnOff = function(data) {
+			$.ajax({
+			url: API_BASEURL + "plugin/tasmota",
+			type: "POST",
+			dataType: "json",
+			data: JSON.stringify({
+				command: "turnOff",
+				ip: data.ip(),
+				idx: data.idx()
+			}),
+			contentType: "application/json; charset=UTF-8"
+			});		
+		}
+		
+		self.checkStatus = function(plugIP,plugIDX) {
+            $.ajax({
+                url: API_BASEURL + "plugin/tasmota",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "checkStatus",
+					ip: plugIP,
+					idx: plugIDX
+                }),
+                contentType: "application/json; charset=UTF-8"
+            }).done(function(){
+				self.settings.saveData();
+				});
+        }; 
+		
+		self.disconnectPrinter = function() {
+            $.ajax({
+                url: API_BASEURL + "plugin/tasmota",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "disconnectPrinter"
+                }),
+                contentType: "application/json; charset=UTF-8"
+            });			
+		}
+		
+		self.connectPrinter = function() {
+            $.ajax({
+                url: API_BASEURL + "plugin/tasmota",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "connectPrinter"
+                }),
+                contentType: "application/json; charset=UTF-8"
+            });			
+		}
+		
+		self.sysCommand = function(sysCmd) {
+            $.ajax({
+                url: API_BASEURL + "plugin/tasmota",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "sysCommand",
+					cmd: sysCmd
+                }),
+                contentType: "application/json; charset=UTF-8"
+            });			
+		}
+		
+		self.checkStatuses = function() {
+			ko.utils.arrayForEach(self.settings.settings.plugins.tasmota.arrSmartplugs(),function(item){
+				if(item.ip() !== "") {
+					console.log("checking " + item.ip())
+					self.checkStatus(item.ip());
+				}
+			});
+        };
+    }
+
+    // view model class, parameters for constructor, container to bind to
+    OCTOPRINT_VIEWMODELS.push([
+        tasmotaViewModel,
+
+        // e.g. loginStateViewModel, settingsViewModel, ...
+        ["settingsViewModel","loginStateViewModel"],
+
+        // "#navbar_plugin_tasmota","#settings_plugin_tasmota"
+        ["#navbar_plugin_tasmota","#settings_plugin_tasmota"]
+    ]);
+});
