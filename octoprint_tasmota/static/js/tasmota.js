ed9ace3b098b1ca26cf02bd1324a10d492c81436
jneilliii <jneilliii+github@gmail.com>
2017-11-04 02:05:38 -0400

    0.1.0

--- octoprint_tasmota/static/js/tasmota.js
+++ octoprint_tasmota/static/js/tasmota.js
@@ -68,7 +68,7 @@ $(function() {
 			
 			console.log(data);
 			plug = ko.utils.arrayFirst(self.settings.settings.plugins.tasmota.arrSmartplugs(),function(item){
-				return (item.ip() === data.ip) && (item.idx() === data.idx);
+				return (item.ip() == data.ip) && (item.idx() == data.idx);
 				}) || {'ip':data.ip,'idx':data.idx,'currentState':'unknown','btnColor':'#808080'};
 			
 			if (data.gcodeon && plug.gcodeEnabled()) {
