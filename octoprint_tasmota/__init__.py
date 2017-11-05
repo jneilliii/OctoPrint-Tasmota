71fa04a0327c62d896a80ddbeb785bd1a442ce6b
jneilliii <jneilliii+github@gmail.com>
2017-11-04 21:35:33 -0400

    0.1.0

--- octoprint_tasmota/__init__.py
+++ octoprint_tasmota/__init__.py
@@ -122,6 +122,7 @@ class tasmotaPlugin(octoprint.plugin.SettingsPlugin,
 				response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx)).read().split()[2])
 				chk = response["POWER"]
 			except:
+				response = "Error connecting to %s." % plugip
 				chk = "UNKNOWN"
 				
 			self._tasmota_logger.debug("%s index %s is %s" % (plugip, plugidx, response))
