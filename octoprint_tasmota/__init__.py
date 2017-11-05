b5fb01b7e4b0e2600b191c480c414d5c8db46dd8
jneilliii <jneilliii+github@gmail.com>
2017-11-04 21:02:09 -0400

    0.1.0

--- octoprint_tasmota/__init__.py
+++ octoprint_tasmota/__init__.py
@@ -118,9 +118,13 @@ class tasmotaPlugin(octoprint.plugin.SettingsPlugin,
 	def check_status(self, plugip, plugidx):
 		self._tasmota_logger.debug("Checking status of %s index %s." % (plugip, plugidx))
 		if plugip != "":
-			response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx)).read().split()[2])
 			self._tasmota_logger.debug("%s index %s is %s" % (plugip, plugidx, response))
-			chk = response["POWER"]
+			try:
+				response = json.loads(urllib2.urlopen("http://" + plugip + "/cm?cmnd=Power" + str(plugidx)).read().split()[2])
+				chk = response["POWER"]
+			except:
+				chk = "UNKNOWN"
+			
 			if chk == "ON":
 				self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="on",ip=plugip,idx=plugidx))
 			elif chk == "OFF":
