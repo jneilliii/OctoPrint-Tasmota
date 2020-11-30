016c5d6fb35ab1b76fc1cc7e98f125f861361d2e
jneilliii <jneilliii+github@gmail.com>
2020-11-30 18:30:30 -0500

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_tasmota"
 plugin_name = "OctoPrint-Tasmota"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.8.16rc3"
+plugin_version = "0.8.16"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
